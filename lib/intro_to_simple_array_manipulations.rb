ae0f1c4bbb9742775a9d87af85e13e2e7e9e0096
John Tom Jr <johntjr.22@gmail.com>
2020-03-06 02:37:37 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,32 @@
+def using_push(array,string)
+updated_array = array.push("violet")
+end
+
+def using_unshift(array,string)
+updated_array = array.unshift("Staten Island")  
+end 
+
+def using_pop(array)
+array.pop()
+end
+
+def pop_with_args(array)
+p array.pop(2)
+end
+
+def using_shift(array) 
+array.shift()
+end
+
+def shift_with_args(array)
+array.shift(2)
+end
+
+def using_concat(array,string)
+array.concat(string)
+end
+
+def using_insert(array,string)
+array.insert(4,"Python")
+end
+
